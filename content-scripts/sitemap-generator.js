11a41e7aefac6199d8b5ba37e51d7cd741d46959
Frederick Lee <frederick.v.l@gmail.com>
2020-05-21 19:25:49 -0400

    Added rudimentary mapping of links in lists.

--- content-scripts/sitemap-generator.js
+++ content-scripts/sitemap-generator.js
@@ -10,14 +10,52 @@
   function bodyCleaner(body)
   {
     {
-      let elements = body.querySelectorAll("script, svg, input, :not(a) img");
+      let elements = body.querySelectorAll("style, br, script, svg, input, :not(a) img, pre, code");
       for (let i = elements.length - 1; i >= 0; i--)
       {
         let element = elements[i];
         element.parentNode.removeChild(element);
-      };
+      }
+    }
+    {
+      let elements = body.querySelectorAll("a");
+      for (let i = elements.length - 1; i >= 0; i--)
+      {
+        let element = elements[i];
+        if (!element.getAttribute("href")) // Effectively a span without an href. Replace to make parsing easier.
+          element.outerHTML = "<span>" + element.innerHTML + "</span>";
+        else if (element.getAttribute("href").startsWith('#') ||
+            element.getAttribute("href").startsWith('javascript:')) // These do not contribute to site layout.
+          element.parentNode.removeChild(element);
+        else
+          element.setAttribute("href", element.href); // Make all hrefs absolute.
+      }
+    }
+    {
+      let elements = body.querySelectorAll("*");
+      for (let i = elements.length - 1; i >= 0; i--)
+      {
+        let element = elements[i];
+        element.style = "";
+      }
     }
     return body;
+  }
+  
+  function bodyExtractor(body)
+  {
+    let extracted = "";
+    let elements = body.querySelectorAll("ul, ol");
+    for (let i = 0; i < elements.length; i++)
+    {
+      let element = elements[i];
+      if (element.querySelector("a") !== null)
+      {
+        //console.log(element);
+        extracted += element.outerHTML + '\n';
+      }
+    }
+    return extracted;
   }
   
   browser.runtime.onMessage.addListener(function(request, sender, sendResponse)
@@ -29,9 +67,11 @@
       console.log(request.text);
       break;
     case "request_sitemap":
-      let newBody = bodyCleaner(document.body.cloneNode(true)).innerHTML;
-      console.log(newBody);
-      sendResponse({updated: true, body: newBody});
+      let newBody = bodyCleaner(document.body.cloneNode(true));
+      console.log(newBody.innerHTML);
+      let sitemap = bodyExtractor(newBody);
+      console.log(sitemap);
+      sendResponse({updated: true, body: sitemap});
       break;
     }
   });
